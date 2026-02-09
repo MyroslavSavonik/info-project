eed7e8e28e9146788390279622a4d1785ea1f58e
MyroslavS <Myroslav1510@gmail.com>
2026-02-09 15:53:59 +0200

    images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,8 @@ import myPhoto2 from "../images/my_photo2.jpg"
 export default function Home() {
     return (
         <div className={"homeDiv"}>
-            <img src={myPhoto} id = "image1" alt = "photo"></img>
-            <img src={myPhoto2} id = "image2" alt = "photo"></img>
+            <img src={myPhoto} id = "image1" alt = "This was a photo of me, but it did not load"></img>
+            <img src={myPhoto2} id = "image2" alt = "This was a photo of me, but it did not load"></img>
             <h1>{resumeData.name}</h1>
             <ul className={"contacts"}>
                 {resumeData.contacts.map((c, i) => <li key={i}>{c}</li>)}
