73926661d710dbf98893494931fbd9b578517cb0
MyroslavS <Myroslav1510@gmail.com>
2026-02-09 15:49:46 +0200

    images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
 
 import { resumeData } from "../d/data.js";
+import myPhoto from "../images/my_photo.jpg";
+import myPhoto2 from "../images/my_photo2.jpg"
 
 export default function Home() {
     return (
         <div className={"homeDiv"}>
-
+            <img src={myPhoto} id = "image1"></img>
+            <img src={myPhoto2} id = "image2"></img>
             <h1>{resumeData.name}</h1>
             <ul className={"contacts"}>
                 {resumeData.contacts.map((c, i) => <li key={i}>{c}</li>)}
             </ul>
+
             <h3>{resumeData.title}</h3>
 
         </div>
